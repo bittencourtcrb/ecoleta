3a5f809d118b8976c8a27f66e1bf2658f02d4dca
unknown <bittencourtcrb@gmail.com>
2020-07-31 00:11:40 -0300

    segunda aula finalizada

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 
+import PointsController from './controller/PointsController';
+import ItemsController from './controller/ItemsController';
+
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/', (request, response) => {
+routes.get('/items', itemsController.index);
 
-    response.json([
-        'Carlos',
-        'Rogerio',
-        'Bittencourt'
-    ])
-})
+routes.post('/points',  pointsController.create);
+routes.get('/points',  pointsController.index);
+routes.get('/points/:id',  pointsController.show);
 
-export default routes;
+export default routes; 
